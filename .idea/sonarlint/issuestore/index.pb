22f72824b36ce546aaab706f5e9e981000ed5bf9
Kelvin Pereira Biserra <42652204+Kelvin-Pereira@users.noreply.github.com>
2023-01-22 19:34:59 -0300

    Refactoring Functions

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -6,11 +6,3 @@
 examples.iml,9\9\996e66bef2f28b5e1a12cfe71a7f36db5b66df74
 =
 src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
-A
-src/map/Main.java,8\a\8a259ff3afa621d090c915309294c6106b1b2e0c
-D
-src/stream/Main.java,b\e\be6254d5ece0ce30c329ff1e9ff2d1a7ff875bfa
-M
-src/stream/objets/Person.java,1\d\1d93a6da1866d683a94d08b31665fc3b6695e793
-L
-src/functions/Functions.java,2\9\29255ce8e281b51e01da6636fbb0a675a9b58a3a
--- src/functions/Functions.java
+++ src/functions/Functions.java
@@ -1,18 +1,18 @@
 package functions;
 
-import java.util.function.Function;
+import java.util.function.IntUnaryOperator;
 
 public class Functions {
 
     public static void main(String[] args) {
-        Function<Integer, Integer> square = x -> x * x;
+        IntUnaryOperator square = x -> x * x;
         int result = process(square, 5);
 //        int result = process((x -> x * x), 5);
         System.out.println(result); // 25
     }
 
-    public static int process(Function<Integer, Integer> func, int num) {
-        return func.apply(num);
+    public static int process(IntUnaryOperator func, int num) {
+        return func.applyAsInt(num);
     }
 
 }
